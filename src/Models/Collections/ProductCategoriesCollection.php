86929ff3b1b46b2a5679a5fa0535b62e349c1265
Michael Bouclas <mbouclas@gmail.com>
2017-02-01 12:55:55 +0200

    Added product count to the categories collection

--- src/Models/Collections/ProductCategoriesCollection.php
+++ src/Models/Collections/ProductCategoriesCollection.php
@@ -2,6 +2,7 @@
 
 namespace Mcms\Products\Models\Collections;
 
+use DB;
 use Kalnoy\Nestedset\Collection;
 
 /**
@@ -12,5 +13,62 @@ use Kalnoy\Nestedset\Collection;
  */
 class ProductCategoriesCollection extends Collection
 {
+    public function countItems()
+    {
+        if ($this->count() == 0) {
+            return $this;
+        }
 
+        $items = new Collection();
+
+        foreach ($this->items as $item) {
+            $items->push([
+                'id' => $item->id,
+                'title' => $item->title,
+                'slug' => $item->slug,
+                'url' => $item->getSlug(),
+                'children' => $item->descendants->pluck('id')
+            ]);
+        }
+
+        $ids = [];
+        $sql = [];
+
+        foreach ($items as $item) {
+            $ids[] = $item['id'];
+            $qm = ['?'];
+
+            if ($item['children']->count() > 0){
+                foreach ($item['children'] as $child) {
+                    $qm[] = '?';
+                    $ids[] = $child;
+                }
+            }
+
+            $sql[] = "(SELECT count(*) as total from product_product_category
+             INNER JOIN products ON (products.id=product_product_category.product_id)
+             where active = 1 AND product_category_id IN (".
+                implode(',',$qm)
+                .")) as c{$item['id']}";
+        }
+
+        $sql = implode(',', $sql);
+
+        $res = DB::select("SELECT {$sql}",  $ids);
+        $ret = new Collection();
+
+        foreach ($res[0] as $index => $count){
+            $id = trim($index, 'c');
+            $collection = new Collection();
+            $thisCat = $items->where('id', (int) $id)->first();
+            foreach ($thisCat as $key => $value) {
+                $collection->{$key} = $value;
+            }
+            $collection->count = $count;
+            $ret[$id] = $collection;
+        }
+
+
+        return $ret;
+    }
 }
